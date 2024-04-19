841f0471bb98ccd15f071165c7053c5240504f24
HarshKrishnaMoye <harshmoye03@gmail.com>
2024-04-20 03:00:16 +0530

    first commit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
     const sendChatBtn = document.querySelector("#send-btn");
 
     let userMessage = null;
-    const API_KEY = "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";
+    const API_KEY = "--PASTE YOUR KEY HERE--";
     const inputInitHeight = chatInput.scrollHeight;
 
     const createChatLi = (message, className) => {
